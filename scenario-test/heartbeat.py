e0a44ec3f9c247f43b58589c1297606ee49b1f11
Vu Phan <vuphan@VUPHAN-PC.fritz.box>
2022-10-24 08:06:46 +0200

    add main function

--- scenario-test/heartbeat.py
+++ scenario-test/heartbeat.py
@@ -1,26 +1,58 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
+
 import pytest
+import os
 #from selenium.webdriver import ActionChains
 
-s=Service('/usr/bin/chromedriver')
+# Get environment variables
+chromdrv = os.getenv('CHROMEDRV') # path to chromedrv
+baseURL = os.getenv('BASEURL') # path to chromedrv
+
+# s=Service('/usr/bin/chromedriver')
+s=Service(chromdrv)
 driver=webdriver.Chrome(service=s)
  
 def test_heartbeat():
-    url = 'http://localhost:8080/heartbeat'
+    print("starting test heartbeat...")
+    url = baseURL + "/heartbeat"
     driver.get(url)
     driver.maximize_window()
     sleep(2)
+
+    # check if the expected result equals the actual
+    # ...
+    
+    # close browser
+    driver.quit()
     
 
 def test_heartbeatWrong():
-    url= 'http://localhost:8080/heartbeat1'
+    print ("start test heartbeat with wrong url...")
+    url = baseURL + "/heartbeat"
     driver.get(url)
     sleep(2)
+    
+    # check if the expected result equals the actual
+    # ...
+    
+    # close browser
+    driver.quit()
    
    
+if __name__ == "__main__":
+    from selenium.common.exceptions import NoSuchElementException
 
+    try:
+        test_heartbeat()
+        test_heartbeatWrong()
+    except:
+        print("Exception: unexpected error! Exit the test program... ")
+        driver.quit()
+
+    
 
 
 
